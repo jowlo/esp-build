2f4fe81658cdfcd49ffe4b30b569fd815638e5cb
Malachi Burke <malachi.burke@gmail.com>
2017-06-21 02:03:46 -0700

    Adding automated date

--- setenv.sh
+++ setenv.sh
@@ -3,9 +3,9 @@
 # repo/tag coming from setenv
 export BASE_IMAGE=$REPO:$TAG
 
+DATE=`date +%Y.%m.%d`
 export REPO=malachib/esp-open-rtos
-#BASE_IMAGE=malachib/esp-open-sdk:2017.06.20
-export TAG_PREFIX=2017.06.20
+export TAG_PREFIX=$DATE
 export TAG_SUFFIX=mal-lwip
 export TAG=$TAG_PREFIX-$TAG_SUFFIX
 export GIT_REPO=https://github.com/malachi-iot/esp-open-rtos.git
