bf9c470d3ef70fbc9b54243b0f1d6d0e86d91838
Malachi Burke <malachi.burke@gmail.com>
2017-06-21 01:32:35 -0700

    Pointing repos at ourairquality code

--- setenv.sh
+++ setenv.sh
@@ -4,9 +4,8 @@
 export BASE_IMAGE=$REPO:$TAG
 
 export REPO=malachib/esp-open-rtos
-#BASE_IMAGE=malachib/esp-open-sdk:2017.06.20
-export TAG_PREFIX=2017.06.20
-export TAG_SUFFIX=mal-lwip
+export TAG_PREFIX=2017.06.21
+export TAG_SUFFIX=lwip2
 export TAG=$TAG_PREFIX-$TAG_SUFFIX
-export GIT_REPO=https://github.com/malachi-iot/esp-open-rtos.git
+export GIT_REPO=https://github.com/ourairquality/esp-open-rtos.git
 export GIT_BRANCH=lwip2
